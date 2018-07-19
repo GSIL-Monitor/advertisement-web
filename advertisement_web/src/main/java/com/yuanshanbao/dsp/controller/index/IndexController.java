bed4f5bf247f398b06d2350b8d611328a9d13052
liudonghaoran <liudonghaoran@yuanshanbao.com>
2018-07-19 10:15:48 +0800

    app首页

--- advertisement_web/src/main/java/com/yuanshanbao/dsp/controller/index/IndexController.java
+++ advertisement_web/src/main/java/com/yuanshanbao/dsp/controller/index/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,27 +14,51 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.yuanshanbao.common.exception.BusinessException;
 import com.yuanshanbao.common.ret.ComRetCode;
 import com.yuanshanbao.common.util.LoggerUtil;
-import com.yuanshanbao.paginator.domain.PageBounds;
+import com.yuanshanbao.dsp.activity.model.Activity;
+import com.yuanshanbao.dsp.config.ConfigManager;
 import com.yuanshanbao.dsp.controller.base.BaseController;
+import com.yuanshanbao.dsp.core.CommonStatus;
 import com.yuanshanbao.dsp.core.InterfaceRetCode;
+import com.yuanshanbao.dsp.product.model.Product;
+import com.yuanshanbao.dsp.product.model.ProductType;
+import com.yuanshanbao.dsp.product.model.vo.ProductVo;
+import com.yuanshanbao.dsp.product.service.ProductService;
+import com.yuanshanbao.paginator.domain.PageBounds;
+import com.yuanshanbao.paginator.domain.PageList;
 
 @RequestMapping({ "/index", "/i/index" })
 @Controller
 public class IndexController extends BaseController {
 
+	private static final String EDUCATION_APP = "education_app";
+
+	@Autowired
+	private ProductService productService;
+
 	// 客户端首页
 	@RequestMapping("/home")
 	@ResponseBody
-	public Object index(HttpServletRequest request,
-			HttpServletResponse response, String activityKey,
+	public Object index(HttpServletRequest request, HttpServletResponse response, String activityKey, Product product,
 			PageBounds pageBounds, Integer client) {
 		Map<String, Object> resultMap = new HashMap<>();
 		try {
-
+			Activity activity = ConfigManager.getActivityByKey(EDUCATION_APP);
+			if (activity == null) {
+				throw new BusinessException();
+			}
+			// 产品列表
+			product.setActivityId(activity.getActivityId());
+			product.setType(ProductType.ONLINE);
+			product.setStatus(CommonStatus.ONLINE);
+			PageList<Product> productList = (PageList<Product>) productService.selectProducts(product,
+					formatPageBounds(pageBounds));
+			PageList<ProductVo> voList = convertVo(request, productList, pageBounds);
+			voList.setPaginator(productList.getPaginator());
+			resultMap.put("productList", voList);
+			resultMap.put(ComRetCode.PAGINTOR, productList.getPaginator());
 			InterfaceRetCode.setAppCodeDesc(resultMap, ComRetCode.SUCCESS);
 		} catch (BusinessException e) {
-			InterfaceRetCode.setSpecAppCodeDesc(resultMap, e.getReturnCode(),
-					e.getMessage());
+			InterfaceRetCode.setSpecAppCodeDesc(resultMap, e.getReturnCode(), e.getMessage());
 		} catch (Exception e) {
 			LoggerUtil.error("[home index]: ", e);
 			InterfaceRetCode.setAppCodeDesc(resultMap, ComRetCode.FAIL);
