606071d1e44967736a07f514a3aca95315ac1035
liudonghaoran <liudonghaoran@yuanshanbao.com>
2018-11-05 20:06:38 +0800

    【蚂蚁智投官网】

--- advertisement_web/src/main/java/com/yuanshanbao/dsp/controller/index/IndexController.java
+++ advertisement_web/src/main/java/com/yuanshanbao/dsp/controller/index/IndexController.java
@@ -4,25 +4,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.yuanshanbao.common.util.DateUtils;
-import com.yuanshanbao.dsp.app.service.AppService;
-import com.yuanshanbao.dsp.message.model.Message;
-import com.yuanshanbao.dsp.message.service.MessageService;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.yuanshanbao.common.exception.BusinessException;
 import com.yuanshanbao.common.ret.ComRetCode;
 import com.yuanshanbao.common.util.LoggerUtil;
 import com.yuanshanbao.dsp.activity.model.Activity;
 import com.yuanshanbao.dsp.advertisement.model.AdvertisementPosition;
+import com.yuanshanbao.dsp.app.service.AppService;
 import com.yuanshanbao.dsp.config.ConfigConstants;
 import com.yuanshanbao.dsp.config.ConfigManager;
 import com.yuanshanbao.dsp.controller.base.BaseController;
 import com.yuanshanbao.dsp.core.InterfaceRetCode;
+import com.yuanshanbao.dsp.message.model.Message;
+import com.yuanshanbao.dsp.message.service.MessageService;
 import com.yuanshanbao.dsp.product.model.Product;
 import com.yuanshanbao.dsp.product.model.ProductCategory;
 import com.yuanshanbao.dsp.product.model.ProductStatus;
@@ -61,10 +65,10 @@ public class IndexController extends BaseController {
 			product.setStatus(ProductStatus.ONLINE);
 			PageList<Product> productList = (PageList<Product>) productService.selectProducts(product,
 					formatPageBounds(pageBounds));
-			//滚动消息列表
+			// 滚动消息列表
 			Message message = new Message();
 			message.setProductId(activity.getActivityId());
-            List<Message> messageList  = messageService.selectMessages(message, new PageBounds());
+			List<Message> messageList = messageService.selectMessages(message, new PageBounds());
 			String channel = appService.getAppChannel(request.getParameter("appId"));
 			String appKey = getAppKey(request);
 			Long activityId = null;
@@ -79,8 +83,8 @@ public class IndexController extends BaseController {
 			resultMap.put("productCategorys", productCategorys);
 			resultMap.put(ComRetCode.PAGINTOR, productList.getPaginator());
 			setAdvertisement(client, resultMap, channel, appKey, activityId, AdvertisementPosition.ADVERTISEMENT_INDEX);
-            resultMap.put("messageList",messageList);
-            resultMap.put("productList", productList);
+			resultMap.put("messageList", messageList);
+			resultMap.put("productList", productList);
 			InterfaceRetCode.setAppCodeDesc(resultMap, ComRetCode.SUCCESS);
 		} catch (BusinessException e) {
 			InterfaceRetCode.setSpecAppCodeDesc(resultMap, e.getReturnCode(), e.getMessage());
@@ -89,6 +93,23 @@ public class IndexController extends BaseController {
 			InterfaceRetCode.setAppCodeDesc(resultMap, ComRetCode.FAIL);
 		}
 		return resultMap;
+	}
+
+	@RequestMapping("/index")
+	public String indexWeb(HttpServletRequest request, ModelMap resultMap) {
+		try {
+			String host = request.getHeader("Host");
+			if (StringUtils.isNotBlank(host) && host.contains("d.xindk.com")) {
+				return getFtlPath(request, "/site/myzt");
+			}
+			return redirect("/404.html");
+		} catch (BusinessException e) {
+			InterfaceRetCode.setAppCodeDesc(resultMap, e.getReturnCode(), e.getMessage());
+		} catch (Exception e) {
+			InterfaceRetCode.setAppCodeDesc(resultMap, ComRetCode.FAIL);
+			LoggerUtil.error("[Result: error]", e);
+		}
+		return getFtlPath(request, "/common/error");
 	}
 
 }
