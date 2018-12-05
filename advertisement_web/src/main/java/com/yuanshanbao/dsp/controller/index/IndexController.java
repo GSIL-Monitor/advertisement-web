95deaf65cb8741cbad314cb8f27c4447f2894175
marui <1441642267@qq.com>
2018-12-05 14:07:05 +0800

    【首页】修改版本号获取

--- advertisement_web/src/main/java/com/yuanshanbao/dsp/controller/index/IndexController.java
+++ advertisement_web/src/main/java/com/yuanshanbao/dsp/controller/index/IndexController.java
@@ -25,7 +25,6 @@ import com.yuanshanbao.dsp.app.service.AppService;
 import com.yuanshanbao.dsp.config.ConfigConstants;
 import com.yuanshanbao.dsp.config.ConfigManager;
 import com.yuanshanbao.dsp.controller.base.BaseController;
-import com.yuanshanbao.dsp.core.IniBean;
 import com.yuanshanbao.dsp.core.InterfaceRetCode;
 import com.yuanshanbao.dsp.core.http.HttpServletRequestWrapper;
 import com.yuanshanbao.dsp.message.model.Message;
@@ -79,11 +78,15 @@ public class IndexController extends BaseController {
 				getHomeInfos(resultMap, activity, product, pageBounds, request, client);
 			} else {
 				// User loginToken = tokenService.verifyLoginToken(token);
-				if (version != null && version.equals(IniBean.getIniValue("wzxcxProductChannel"))) {
-					resultMap.put("version", true);
-				} else {
+				if (StringUtils.isNotBlank(version) && "0.3.7".equals(version)) {
 					resultMap.put("version", false);
 				}
+				/*
+				 * if (version != null &&
+				 * version.equals(IniBean.getIniValue("wzxcxProductChannel"))) {
+				 * resultMap.put("version", true); } else {
+				 * resultMap.put("version", false); }
+				 */
 				HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(token, request);
 				User user = (User) requestWrapper.getSession().getAttribute(SessionConstants.SESSION_ACCOUNT);
 
